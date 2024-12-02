dc94cee60fdf219e0c69bc4ad8103159fc31a242
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-12-02 16:55:16 +0100

    Dynamic Cards

--- frontend/src/Components/Card/Card.tsx
+++ frontend/src/Components/Card/Card.tsx
@@ -1,9 +1,17 @@
 import React from "react";
 import "./Card.css";
 
-type Props = {};
+interface Props {
+  companyName: string;
+  ticker: string;
+  price: number;
+}
 
-const Card = (props: Props) => {
+const Card: React.FC<Props> = ({
+  companyName,
+  ticker,
+  price,
+}: Props): JSX.Element => {
   return (
     <div className="card">
       <img
@@ -11,8 +19,10 @@ const Card = (props: Props) => {
         alt="image"
       />
       <div className="details">
-        <h2>AAPL</h2>
-        <p>110$</p>
+        <h2>
+          {companyName} ({ticker})
+        </h2>
+        <p>€{price}</p>
       </div>
       <p className="info">
         Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, nam!
