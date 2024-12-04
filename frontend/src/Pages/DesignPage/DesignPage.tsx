24e128b4d900a103dd668908d742ced562e90671
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-12-04 14:09:23 +0100

    Company Profile

--- frontend/src/Pages/DesignPage/DesignPage.tsx
+++ frontend/src/Pages/DesignPage/DesignPage.tsx
@@ -1,14 +1,23 @@
 import React from "react";
 import Table from "../../Components/Table/Table";
 import RatioList from "../../Components/RatioList/RatioList";
+import { testIncomeStatementData } from "../../Components/Table/testData";
 
 type Props = {};
+
+const tableConfig = [
+  {
+    label: "Market Cap",
+    render: (company: any) => company.marketCapTTM,
+    subTitle: "Total value of all a company's shares of stock",
+  },
+];
 
 const DesignPage = (props: Props) => {
   return (
     <>
       <h1>CryptoCash Design Page</h1>
-      <RatioList />
+      <RatioList data={testIncomeStatementData} config={tableConfig} />
       <h2>
         This is CryptoCash's design page. This is where we will house various
         design aspects of the app
