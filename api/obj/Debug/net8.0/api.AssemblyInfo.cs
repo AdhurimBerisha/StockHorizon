dbad1cb7e4f5187dfd74dcda5096a0075651e8c6
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-12-06 11:41:10 +0100

    Comment GET + Include()

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b35a038b5676cb5d33a5e6b943621b0b36ae9cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dca1c1c83a18b6dfd3065a3b88ed5a20476ff7bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
