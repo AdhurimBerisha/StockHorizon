36083b04731afd9dc2628a40fe9c318c8775d97b
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-12-09 20:17:50 +0100

    Services

--- frontend/src/Components/CashFlowStatement/CashFlowStatement.tsx
+++ frontend/src/Components/CashFlowStatement/CashFlowStatement.tsx
@@ -4,7 +4,7 @@ import { useOutletContext } from "react-router";
 import { getCashFlowStatement } from "../../api";
 import Table from "../Table/Table";
 import Spinner from "../Spinner/Spinner";
-import { formatLargeMonetaryNumber } from "../Helpers/NumberFormatting";
+import { formatLargeMonetaryNumber } from "../../Helpers/NumberFormatting";
 
 type Props = {};
 
