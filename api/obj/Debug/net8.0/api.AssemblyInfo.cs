793e6be934415464f7025dbebb976109af18e12a
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-12-09 15:21:31 +0100

    Finished API

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a05c4f485014ab52e3ea6a3739158c0b318e784")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3cfe07b77b67ea68a1407d562c59edd91ced20")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
