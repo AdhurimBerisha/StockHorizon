7126900775071b1f3a4d6de116ba71936681c5db
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-12-09 12:36:14 +0100

    Portfolio GET

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+378399caddd091b00b94e95c078ecbc01056636e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6d0246099a22aa4853075ef982b28d7ab6cf980")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
