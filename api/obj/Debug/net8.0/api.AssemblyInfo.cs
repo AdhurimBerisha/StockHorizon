27ecc8e179deceeeddd31dac69e7e5a864fa9f99
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-12-10 12:32:19 +0100

    Protected Routes

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df4b495f3d439d748e194ac2f3bd05abd391b24")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e74aa8effdfdb5da2820f8d2c55f8083209519b2")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
