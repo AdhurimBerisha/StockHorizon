f9b5b68c032539f592e0e6b80cac4309ccfd733f
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-12-10 12:06:36 +0100

    Login

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c3cfe07b77b67ea68a1407d562c59edd91ced20")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0df4b495f3d439d748e194ac2f3bd05abd391b24")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
