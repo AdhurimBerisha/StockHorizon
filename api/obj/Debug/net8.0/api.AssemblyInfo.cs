b24218ba6d6c690d79ba1c91d1cb816bf3f132ec
AdhurimBerisha <138231376+AdhurimBerisha51865@users.noreply.github.com>
2024-12-12 17:47:31 +0100

    Portfolio Persisted

--- api/obj/Debug/net8.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ffbefddac46ffff73415ec72df60e53a38b15e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3750fbc9d5578d44823b9d6d560c10620832645")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
